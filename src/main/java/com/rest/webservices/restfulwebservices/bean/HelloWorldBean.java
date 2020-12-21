1f939d0f77367846310a8c2873a5c0c1b20fa563
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-21 20:18:56 +0530

    Added service to return Hello World Bean

---
+++ src/main/java/com/rest/webservices/restfulwebservices/bean/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.rest.webservices.restfulwebservices.bean;
+
+public class HelloWorldBean {
+	String message;
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
--- src/main/java/com/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rest.webservices.restfulwebservices.bean.HelloWorldBean;
+
 @RestController
 public class HelloWorldController {
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
 	}
 }
