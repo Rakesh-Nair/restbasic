ced8a539d42d76e0452038dda3b1ef555e348038
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-29 17:53:33 +0530

    Added some API documentation

--- src/main/java/com/rest/webservices/restfulwebservices/bean/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/bean/User.java
@@ -1,63 +1,75 @@
 package com.rest.webservices.restfulwebservices.bean;
 
 import java.util.Date;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "All details about User")
 public class User {
 	private Integer id;
-	@Size(min=2, message="Name should be atleast 2 characters long")
+	@Size(min = 2, message = "Name should be atleast 2 characters long")
+	@ApiModelProperty(notes = "Name should have atleast 2 characters")
 	private String name;
 	@Past()
+	@ApiModelProperty(notes = "Birth date should be a Past date")
 	private Date birthDate;
-	
-	
+
 	public User(Integer id, String name, Date birthDate) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.birthDate = birthDate;
 	}
-	
+
 	/**
 	 * @return the id
 	 */
 	public Integer getId() {
 		return id;
 	}
+
 	/**
 	 * @param id the id to set
 	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	/**
 	 * @return the birthDate
 	 */
 	public Date getBirthDate() {
 		return birthDate;
 	}
+
 	/**
 	 * @param birthDate the birthDate to set
 	 */
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
 	}
-	
+
 }
